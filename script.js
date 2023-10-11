c31fca7eff414afdc86fb1f138c2545a25af706e
Elijah <iamngunjiri@gmail.com>
2023-10-11 12:29:30 +0300

    Made the navbar responsive

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     var currentYear = new Date().getFullYear();
     document.getElementById('copyright-year').textContent = currentYear;
 });
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById("navbar");
+const close = document.getElementById('close')
+
+if (bar){
+    bar.addEventListener("click", ()=>{
+        nav.classList.add('active');
+    })
+}
+
+if (close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active')
+    })
+}
