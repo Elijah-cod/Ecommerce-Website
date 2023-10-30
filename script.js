ef5bfa6b65fa765135e5ff727a64b2fec1146adb
Elijah <iamngunjiri@gmail.com>
2023-10-30 10:47:31 +0300

    Created a product description page and made it responsive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Making the navbar responsive
 document.addEventListener('DOMContentLoaded', function() {
     var currentYear = new Date().getFullYear();
     document.getElementById('copyright-year').textContent = currentYear;
@@ -17,4 +18,25 @@ if (close){
     close.addEventListener('click', ()=>{
         nav.classList.remove('active')
     })
+}
+
+
+// Changing the images in the products page
+var MainImage = document.getElementById('MainImg')
+var smallImage = document.getElementsByClassName('small-img')
+
+smallImage[0].onclick = function(){
+    MainImage.src = smallImage[0].src
+}
+
+smallImage[1].onclick = function(){
+    MainImage.src = smallImage[1].src
+}
+
+smallImage[2].onclick = function(){
+    MainImage.src = smallImage[2].src
+}
+
+smallImage[3].onclick = function(){
+    MainImage.src = smallImage[3].src
 }
